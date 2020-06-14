f6313e6a7e0f9a8b6094b33a1cdb5e197e48132e
ctorek <ctorek@protonmail.ch>
2020-06-13 21:27:46 -0400

    Started text form on edit page

--- lib/views/edit.dart
+++ lib/views/edit.dart
@@ -1,3 +1,4 @@
+import 'package:validators/validators.dart';
 import 'package:flutter/material.dart';
 
 import "../services/storage.dart";
@@ -8,6 +9,15 @@ class EditPage extends StatefulWidget {
 }
 
 class _EditPageState extends State<EditPage> {
+  final _editFormKey = GlobalKey<FormState>();
+  final _editController = TextEditingController();
+
+  @override
+  void dispose() {
+    _editController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,42 +25,70 @@ class _EditPageState extends State<EditPage> {
         // Back button in AppBar automatically added
         title: Text("Add New RSS Source"),
       ),
-      body: Scaffold(
-        body: FutureBuilder(
-          builder: (BuildContext ctx, AsyncSnapshot snapshot) {
-            if (snapshot.hasData && snapshot.connectionState == ConnectionState.done) {
-              return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (BuildContext ctxt, int index) {
-                  return ListTile(
-                    leading: Icon(Icons.rss_feed),
-                    title: snapshot.data[index],
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {
-                        setState(() {
-                          deleteFromList("", index);
-                        });
-                      },
-                    ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Form(
+            key: _editFormKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  controller: _editController,
+                  validator: (value) {
+                    if (!isURL(value)) {
+                      return "Value is not a valid URL.";
+                    }
+                    Scaffold.of(context).showSnackBar(
+                      SnackBar(content: Text("RSS source successfully added!"))
+                    );
+                    return null;
+                  },
+                )
+              ]
+            )
+          ),
+          Container(
+            child: FutureBuilder(
+              builder: (BuildContext ctx, AsyncSnapshot snapshot) {
+                if (snapshot.hasData && snapshot.connectionState == ConnectionState.done) {
+                  return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext ctxt, int index) {
+                      return ListTile(
+                        leading: Icon(Icons.rss_feed),
+                        title: snapshot.data[index],
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            setState(() {
+                              deleteFromList("feeds", index);
+                            });
+                          },
+                        ),
+                      );
+                    },
                   );
-                },
-              );
-            }
-            return Align(
-              alignment: Alignment.center,
-              child: CircularProgressIndicator()
-            );
-          },
-          future: getList("")
-        ),
+                }
+                return Align(
+                  alignment: Alignment.center,
+                  child: CircularProgressIndicator()
+                );
+              },
+              future: getList("feeds")
+            )
+          )
+        ]
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.check),
         onPressed: () {
           // Save new list of RSS feeds, go back to home
-          // TODO: saving new feeds
-          Navigator.pop(context);
+          if (_editFormKey.currentState.validate()) {
+            setState(() {
+              addToList("feeds", _editController.text);
+            });
+            Navigator.pop(context);
+          }
         }
       ),
     );
