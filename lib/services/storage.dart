91769df384bbea2ea9eb5e487a648fb309d08d10
ctorek <ctorek@protonmail.ch>
2020-07-09 12:59:12 -0400

    Added database

--- lib/services/storage.dart
+++ lib/services/storage.dart
@@ -2,6 +2,25 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import 'dart:async';
 
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<Database> database;
+
+Future<void> initDatabase() async {
+  if (database != null) return;
+  database = openDatabase(
+    join(await getDatabasesPath(), "urls.db"),
+    version: 1,
+    onCreate: (db, version) {
+      return db.execute(
+        "CREATE TABLE urls(id INTEGER PRIMARY KEY, url TEXT, type TEXT)"
+      );
+    }
+  );
+}
+
 Future<void> saveList(String listName, List<String> list) async {
   final prefs = await SharedPreferences.getInstance();
   prefs.setStringList(listName, list);
