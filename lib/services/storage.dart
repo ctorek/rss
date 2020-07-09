7e4d4232521803d73404846bc000b75335f2bc56
ctorek <ctorek@protonmail.ch>
2020-07-09 13:08:23 -0400

    Added sources class

--- lib/services/storage.dart
+++ lib/services/storage.dart
@@ -6,6 +6,22 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+class Source {
+  int id;
+  String url;
+  String type;
+
+  Source({this.id, this.url, this.type});
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "url": url,
+      "type": type
+    };
+  }
+}
+
 Future<Database> database;
 
 Future<void> initDatabase() async {
